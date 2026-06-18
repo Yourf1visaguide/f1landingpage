b824e8e1d2418b8f8f54c87557e6f62b2d0d480a
amardeep0851 <amardeep0851@gmail.com>
2026-06-18 07:01:06 +0000

    add metadata

--- components/MainPage/HeroSection/HeroSection.tsx
+++ components/MainPage/HeroSection/HeroSection.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
     <section className="relative min-h-screen bg-[#fafafa] overflow-hidden mt-16 ">
       {/* Subtle gradient accent */}
       <div className="absolute top-0 right-0 w-full h-full bg-linear-to-l to-zinc-900 via-zinc-900 from-transparent opacity-80  z-10" />
-      <Image src="/images/hero-section-bg.png" fill alt="Background Image"  className="z-0 object-cover object-top"/>
+      <Image src="/images/hero-section-bg.jpg" fill alt="Background Image"  className="z-0 object-cover object-top"/>
       <div className="relative max-w-7xl mx-auto px-6 lg:px-8 pt-12 pb-12 lg:pt-16 lg:pb-16 ">
         <div className="flex flex-col">
           
