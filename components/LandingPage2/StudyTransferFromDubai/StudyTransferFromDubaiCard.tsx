d9a2486b83da1e7fc18def73397c3524969c4020
amardeep0851 <amardeep0851@gmail.com>
2026-07-15 04:44:25 +0000

    dubai landing page typo change.

--- components/LandingPage2/StudyTransferFromDubai/StudyTransferFromDubaiCard.tsx
+++ components/LandingPage2/StudyTransferFromDubai/StudyTransferFromDubaiCard.tsx
@@ -29,7 +29,7 @@ export default function StudyTransferFromDubaiCard({
       </div>
 
       <h3 className="max-w-10 min-[360px]:max-w-20 sm:max-w-32 text-wrap text-center max-[260px]:text-[12px] text-sm font-semibold text-zinc-800">
-        2nd Year {item.name}
+        2nd Year In {item.name}
       </h3>
     </div>
   );
